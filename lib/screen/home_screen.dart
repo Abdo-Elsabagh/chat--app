959b4074e70fed837098acb0e4c07c8612a11fe6
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2025-08-07 00:21:44 +0300

    `Refactor Home Screen: Replace Scaffold with FutureBuilder`

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,69 +12,80 @@ class ChatScreen extends StatelessWidget {
   TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              KLogo,
-              height: 70,
+    return FutureBuilder<QuerySnapshot>(
+      future: messages.get(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data!.docs[0]['message']);
+          return Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    KLogo,
+                    height: 70,
+                  ),
+                  const Text(
+                    'Chat',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Pacifico',
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              backgroundColor: kPrimaryColors,
             ),
-            const Text(
-              'Chat',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: 'Pacifico',
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
+            body: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: false,
+                    itemBuilder: (context, index) {
+                      return ChatBuble(
+                        backgrange: kPrimaryColors,
+                        text: 'i am new user ',
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TextField(
+                    controller: controller,
+                    onSubmitted: (data) {
+                      messages.add({KMessages: data});
+                      controller.clear();
+                    },
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.black12,
+                        suffixIcon: const Icon(Icons.send,
+                            color: kPrimaryColors, size: 30),
+                        hintText: 'Send Massage',
+                        hintStyle: const TextStyle(color: Colors.black),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(
+                                color: kPrimaryColors, width: 2)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16),
+                            borderSide: const BorderSide(
+                                color: Colors.teal, width: 2))),
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
-        backgroundColor: kPrimaryColors,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: false,
-              itemBuilder: (context, index) {
-                return ChatBuble(
-                  backgrange: kPrimaryColors,
-                  text: 'i am new user ',
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: TextField(
-              controller: controller,
-              onSubmitted: (data) {
-                messages.add({KMessages: data});
-                controller.clear();
-              },
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.black12,
-                  suffixIcon:
-                      const Icon(Icons.send, color: kPrimaryColors, size: 30),
-                  hintText: 'Send Massage',
-                  hintStyle: const TextStyle(color: Colors.black),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide:
-                          const BorderSide(color: kPrimaryColors, width: 2)),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                      borderSide:
-                          const BorderSide(color: Colors.teal, width: 2))),
-            ),
-          )
-        ],
-      ),
+          );
+        } else {
+          return const Text('Loading.....');
+        }
+      },
     );
   }
 }
